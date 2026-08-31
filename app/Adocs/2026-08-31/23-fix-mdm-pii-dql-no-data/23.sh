4eba128f94c04430b3a879b6b55900e277f95f87
kim <kimdevops90@gmail.com>
2026-08-31 11:30:41 +0900

    all

---
+++ app/Adocs/2026-08-31/23-fix-mdm-pii-dql-no-data/23.sh
@@ -0,0 +1,7 @@
+open "/Users/k/Learnings/AIProject/CursorFiles/Daily Files/2026-08-31/23-fix-mdm-pii-dql-no-data/23-mdm-fixed-queries.dql"
+open "/Users/k/Learnings/AIProject/CursorFiles/Daily Files/2026-08-31/23-fix-mdm-pii-dql-no-data/23-fix-mdm-pii-dql-no-data.md"
+cp "/Users/k/Learnings/AIProject/CursorFiles/Daily Files/2026-08-31/23-fix-mdm-pii-dql-no-data/"* "/Users/k/Codes/Pra/P1GithubActions/P1Gactions/app/Adocs/2026-08-31/23-fix-mdm-pii-dql-no-data/"
+cd "/Users/k/Codes/Pra/P1GithubActions/P1Gactions" && git status
+cd "/Users/k/Codes/Pra/P1GithubActions/P1Gactions" && git add app/Adocs/2026-08-31/23-fix-mdm-pii-dql-no-data
+cd "/Users/k/Codes/Pra/P1GithubActions/P1Gactions" && git commit -m "Add fixed MDM PII DQL for no-data empty results"
+cd "/Users/k/Codes/Pra/P1GithubActions/P1Gactions" && git push
